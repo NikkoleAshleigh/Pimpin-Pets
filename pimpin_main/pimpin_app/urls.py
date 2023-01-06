ab0bec18c0aff12b7211d36f331ed23fcbc78aa6
lederius <lederius.franklin@gmail.com>
2023-01-05 18:19:22 -0700

    lederius- created 2 urls paw & furever

--- pimpin_main/pimpin_app/urls.py
+++ pimpin_main/pimpin_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from pimpin_app.views import HomeView
+from pimpin_app.views import HomeView, FureverView, PawfrenceView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='pimpin'),
+    path('', PawfrenceView.as_view(), name='pawfrence'),
+    path('', FureverView.as_view(), name='furever'),
+
 ]
