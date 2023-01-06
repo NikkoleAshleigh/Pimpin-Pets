9a06471ce78314afc2c3f60c9baf91131f697e27
NikkoleAshleigh <nikkole.hardy.7.1.21@gmail.com>
2023-01-05 19:53:50 -0500

    changes to run server

--- pimpin_main/pimpin_main/settings.py
+++ pimpin_main/pimpin_main/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pimpin_app.Apps.PimpinAppConfig',
+    'pimpin_app.apps.PimpinAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
